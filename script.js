a04955b875a25bea83c57fbcd9a15d6c4936782a
alienshy <alisahverdiyeW8@gmail.com>
2023-11-24 13:55:01 +0400

    jstask10

--- script.js
+++ script.js
@@ -1,27 +1,56 @@
-const showSelected = () => {
-    const radios = document.getElementsByName('selectradio');
-    for (let i = 0; i < radios.length; i++) {
-        if (radios[i].checked) {
-            console.log(`Secilmis: ${radios[i].value}`);
-            break;
-        }
+
+document.body.innerHTML = `
+ <div id="categories" class="div"></div>
+ <button id="addButton" class="sd">Add</button>
+`;
+const divElement = document.getElementById("categories");
+const categories = ["Yaşlılar üçün", "Böyüklər üçün", "Kiçiklər üçün"];
+for (const category of categories) {
+    divElement.innerHTML += `
+     <input type="radio" id="radio" name="category" value="${category}">
+     <label>${category}</label>
+ `;
+}
+const addButton = document.getElementById("addButton");
+addButton.addEventListener("click", addButtonClicked);
+function addButtonClicked() {
+    const selectedRadio = document.querySelector('input[name="category"]:checked');
+    if (selectedRadio) {
+        console.log("kategori: " + selectedRadio.value);
+    } else {
+        console.log("kategori seçin.");
     }
-};
-const textInput = document.getElementById('textInput');
-const closeBtn = document.getElementById('closeclick');
-const openBtn = document.getElementById('openclick');
-closeBtn.addEventListener('click', closeClick);
-openBtn.addEventListener('click', openClick);
-function closeClick() {
-    textInput.value = '';
-    textInput.disabled = true;
-    closeBtn.disabled = true;
-    openBtn.disabled = false;
 }
-function openClick() {
-    textInput.disabled = false;
-    textInput.focus();
-    closeBtn.disabled = false;
-    openBtn.disabled = true;
+
+
+const containerDiv = document.createElement("div");
+containerDiv.className = "div2";
+const inputElement = document.createElement("input");
+inputElement.type = "text";
+inputElement.id = "textInput";
+inputElement.placeholder = "Yazin";
+const openButton = document.createElement("button");
+const closeButton = document.createElement("button");
+closeButton.className = "sf";
+closeButton.textContent = "Close";
+closeButton.onclick = closeInput;
+openButton.className = "sf";
+openButton.textContent = "Open";
+openButton.onclick = openInput;
+openButton.disabled = true;
+
+containerDiv.appendChild(inputElement);
+
+containerDiv.appendChild(closeButton);
+containerDiv.appendChild(openButton);
+document.body.appendChild(containerDiv);
+ function closeInput() {
+    inputElement.disabled = true;
+    closeButton.disabled = true;
+    openButton.disabled = false;
 }
-openClick();
+function openInput() {
+    inputElement.disabled = false;
+    openButton.disabled = true;
+    closeButton.disabled = false;
+}
